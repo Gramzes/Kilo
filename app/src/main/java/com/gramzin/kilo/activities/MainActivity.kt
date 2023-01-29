e398e3039ceaf5fd4a67fb1e9864c11930918546
Gramzes <gramzin4666@gmail.com>
2023-01-29 16:51:55 +0300

    Добавлена модель сообщения.

--- app/src/main/java/com/gramzin/kilo/activities/MainActivity.kt
+++ app/src/main/java/com/gramzin/kilo/activities/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.gramzin.kilo
+package com.gramzin.kilo.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gramzin.kilo.R
 
-class MainActivity : AppCompatActivity() {
+class ainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/gramzin/kilo/model/Message.kt
@@ -0,0 +1,3 @@
+package com.gramzin.kilo.model
+
+data class Message(val name: String, val text: String?, val imageURL: String?)
