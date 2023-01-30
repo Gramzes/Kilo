1de80f81cd338a7d1950839194f893329c26bde8
Gramzes <gramzin4666@gmail.com>
2023-01-30 22:23:46 +0300

    Экран входа доработан

--- app/src/main/java/com/gramzin/kilo/activities/StartActivity.kt
+++ app/src/main/java/com/gramzin/kilo/activities/StartActivity.kt
@@ -1,12 +1,16 @@
 package com.gramzin.kilo.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.gramzin.kilo.R
 import com.gramzin.kilo.databinding.ActivityStartBinding
 import com.gramzin.kilo.viewmodel.StartActivityViewModel
@@ -15,35 +19,114 @@ class StartActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStartBinding
     private val viewModel: StartActivityViewModel by viewModels()
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
+
+        if(checkIsUserSigned()){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         binding.signUpBtn.setOnClickListener {
             viewModel.signInState.value = !viewModel.signInState.value!!
         }
+        binding.signInBtn.setOnClickListener{pressSignInBtn()}
 
         initUI()
         viewModel.signInState.observe(this, onChangeState)
+        setEditTextListeners()
 
-        binding.passEditText.setOnKeyListener { view, i, keyEvent ->
+
+    }
+
+    fun setEditTextListeners() = with(binding){
+        passEditText.setOnKeyListener { view, i, keyEvent ->
             viewModel.password = (view as EditText).text.toString()
             true
         }
-        binding.nameEditText.setOnKeyListener { view, i, keyEvent ->
+        nameEditText.setOnKeyListener { view, i, keyEvent ->
             viewModel.name = (view as EditText).text.toString()
             true
         }
-        binding.emailEditText.setOnKeyListener { view, i, keyEvent ->
+        emailEditText.setOnKeyListener { view, i, keyEvent ->
             viewModel.email = (view as EditText).text.toString()
             true
         }
-        binding.passRepeatEditText.setOnKeyListener { view, i, keyEvent ->
+        passRepeatEditText.setOnKeyListener { view, i, keyEvent ->
             viewModel.repeatPassword = (view as EditText).text.toString()
             true
         }
+    }
+
+    fun pressSignInBtn(){
+        if(viewModel.signInState.value!!){
+            signIn()
+        }
+        else{
+            signUp()
+        }
+    }
+
+    private fun signIn(){
+        val email = binding.emailEditText.text.toString()
+        val password = binding.passEditText.text.toString()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    if(auth.currentUser!!.isEmailVerified) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this, "Вы не подтвердили email.", Toast.LENGTH_LONG).show()
+                    }
+                } else {
+                    Toast.makeText(this, task.exception?.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun signUp(){
+        val email = binding.emailEditText.text.toString()
+        val password = binding.passEditText.text.toString()
+        val repeatPass = binding.passRepeatEditText.text.toString()
+        val name = binding.nameEditText.text.toString()
+        if (email.isEmpty() || password.isEmpty() || repeatPass.isEmpty() || name.isEmpty()){
+            Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+            return
+        }
+        if(password != repeatPass){
+            Toast.makeText(this, "Пароли не совпадают", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    sendVerificationEmail()
+                } else {
+                    Toast.makeText(this, task.exception?.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+
+    private fun sendVerificationEmail() {
+        Toast.makeText(this, "Вам будет отослано письмо для потверждения почты.", Toast.LENGTH_LONG).show()
+        viewModel.signInState.value = true
+        auth.currentUser!!.sendEmailVerification()
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful)
+                    Toast.makeText(this, task.exception?.localizedMessage, Toast.LENGTH_LONG).show()
+            }
     }
 
     private val onChangeState = Observer<Boolean> {
@@ -62,10 +145,14 @@ class StartActivity : AppCompatActivity() {
         }
     }
 
+
     fun initUI() = with(binding){
         emailEditText.setText(viewModel.email)
         nameEditText.setText(viewModel.name)
         passEditText.setText(viewModel.password)
         passRepeatEditText.setText(viewModel.repeatPassword)
     }
+
+    fun checkIsUserSigned() = auth.currentUser != null
+
 }
